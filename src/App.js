5cfec6c30e6381a8b5713297c38577aaac3db60a
Mohamed Es-saadani <essaadani80@gmail.com>
2021-11-06 21:01:38 +0100

    jest testing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
         <Router>
             <div id="wrapper">
-                <h1>Liste des clients</h1>
                 <Sidebar />
                 <div id="content-wrapper" className="d-flex flex-column">
                     <div id="content">
